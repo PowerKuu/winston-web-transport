aa738ab125d1737347d2cd67e1fadd85e0578ff0
Håkon <hakon.kleven@aplia.no>
2024-12-06 08:55:37 +0100

    remove secre

--- test/test.ts
+++ test/test.ts
@@ -2,6 +2,10 @@ import winston from "winston"
 import WebTransport from "../src/main"
 import { join } from "path"
 
+import dotenv from "dotenv"
+
+dotenv.config()
+
 export function createLogger() {
     const logger = winston.createLogger({
         format: winston.format.combine(
@@ -25,10 +29,10 @@ export function createLogger() {
         transports: [
             new winston.transports.Console(),
             new WebTransport({
-                port: 3000,
+                port: 3005,
                 password: "123",
                 postgres: {
-                    connectionUri: "postgres://uagk6ve2oo0pnk:pc7e9378814c0dbdad1710d301170269dfe1d921ae886633e35c57c79a819c30f@c9tiftt16dc3eo.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d3nhjpd2aevjcl?sslmode=require",
+                    connectionUri: process.env.DATABASE_URL,
                     pragnationLimit: 10
                 }
             })
